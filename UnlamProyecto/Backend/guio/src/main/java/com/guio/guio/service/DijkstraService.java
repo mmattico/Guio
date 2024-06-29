88ddefcb3af5ac9c4b8a4d28cc662787244779fd
mmattico <martinmatticoli@gmail.com>
2024-06-29 12:16:49 -0300

    InstruccionesDeGiro

--- UnlamProyecto/Backend/guio/src/main/java/com/guio/guio/service/DijkstraService.java
+++ UnlamProyecto/Backend/guio/src/main/java/com/guio/guio/service/DijkstraService.java
@@ -62,8 +62,71 @@ public class DijkstraService {
     }
 
     public static Camino convertirGrafoACamino(Grafo grafo, String nodoDestinoNombre) {
+        Nodo nodoDestino = getNodoFromGrafo(grafo, nodoDestinoNombre);
+        Camino camino = obtenerInstrucciones(nodoDestino);
+        return camino;
+    }
 
+    private static Camino obtenerInstrucciones(Nodo nodoDestino) {
         Camino camino = new Camino();
+        Instruccion instruccion = getInstruccionInicial(nodoDestino);
+        camino.addInstruccion(instruccion);
+
+        Arista aristaAnterior = new Arista();
+        for (Nodo nodo: nodoDestino.getCaminoCorto()){
+            if(nodo.getArista().getDistancia().compareTo(0) > 0) {
+                if(aristaAnterior.getDistancia().compareTo(0) > 0){
+                    obtenerInstruccionDeDireccion(camino, aristaAnterior, nodo.getArista());
+                }
+                addInstruccionACamino(camino, nodo);
+                aristaAnterior = nodo.getArista();
+            }
+        }
+
+        if(nodoDestino.getArista().getDistancia().compareTo(0) > 0) {
+            if(aristaAnterior.getDistancia().compareTo(0) > 0){
+                obtenerInstruccionDeDireccion(camino, aristaAnterior, nodoDestino.getArista());
+            }
+            addInstruccionACamino(camino, nodoDestino);
+        }
+        return camino;
+    }
+
+    private static void addInstruccionACamino(Camino camino, Nodo nodo) {
+        Instruccion instruccion;
+        instruccion = new Instruccion();
+        instruccion.setCommando("El nodo " + nodo.getNombre() + " esta a una distancia de " + nodo.getArista().getDistancia() + " metros");
+        camino.addInstruccion(instruccion);
+    }
+
+    private static void obtenerInstruccionDeDireccion(Camino camino, Arista aristaAnterior, Arista aristaDestino) {
+        Instruccion instruccion;
+        String orientacionFinal = aristaAnterior.getSentidoDestino();
+        String orientacionDestino = aristaDestino.getSentidoOrigen();
+        if((orientacionFinal.equals("N") && orientacionDestino.equals("O"))
+                || (orientacionFinal.equals("O") && orientacionDestino.equals("S"))
+                || (orientacionFinal.equals("S") && orientacionDestino.equals("E"))
+                || (orientacionFinal.equals("E") && orientacionDestino.equals("N"))){
+            instruccion = new Instruccion();
+            instruccion.setCommando("Girar a la derecha");
+            camino.addInstruccion(instruccion);
+        }else if((orientacionFinal.equals("N") && orientacionDestino.equals("E"))
+                || (orientacionFinal.equals("E") && orientacionDestino.equals("S"))
+                || (orientacionFinal.equals("S") && orientacionDestino.equals("O"))
+                || (orientacionFinal.equals("O") && orientacionDestino.equals("N"))){
+            instruccion = new Instruccion();
+            instruccion.setCommando("Girar a la izquierda");
+            camino.addInstruccion(instruccion);
+        }
+    }
+
+    private static Instruccion getInstruccionInicial(Nodo nodoDestino) {
+        Instruccion instruccion = new Instruccion();
+        instruccion.setCommando("El objetivo esta a una distancia de "+ nodoDestino.getDistancia()+" metros");
+        return instruccion;
+    }
+
+    private static Nodo getNodoFromGrafo(Grafo grafo, String nodoDestinoNombre) {
         Nodo nodoDestino = new Nodo();
         for (Nodo nodo: grafo.getNodos()){
             if(nodo.getNombre().equals(nodoDestinoNombre)){
@@ -71,24 +134,6 @@ public class DijkstraService {
                 break;
             }
         }
-        Instruccion instruccion = new Instruccion();
-        instruccion.setCommando("El objetivo esta a una distancia de "+nodoDestino.getDistancia()+" metros");
-        camino.addInstruccion(instruccion);
-
-        for (Nodo nodo: nodoDestino.getCaminoCorto()){
-            if(nodo.getArista().getDistancia().compareTo(0) > 0) {
-                instruccion = new Instruccion();
-                instruccion.setCommando("El nodo "+nodo.getNombre()+" esta a una distancia de " + nodo.getArista().getDistancia() + " metros");
-                camino.addInstruccion(instruccion);
-            }
-        }
-
-        if(nodoDestino.getArista().getDistancia().compareTo(0) > 0) {
-            instruccion = new Instruccion();
-            instruccion.setCommando("El nodo "+nodoDestino.getNombre()+" esta a una distancia de " + nodoDestino.getArista().getDistancia() + " metros");
-            camino.addInstruccion(instruccion);
-        }
-
-        return camino;
+        return nodoDestino;
     }
 }
