74c9a63b7723370158d252be37d73253e6a2d2a7
joelakachanosqui <59021153+joelakachanosqui@users.noreply.github.com>
2024-07-02 21:09:21 -0300

    Start Page
    Se agrega start page

---
+++ UnlamProyecto/Frontend/guio_proyecto/lib/start_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+//import 'login.dart';
+//import 'signup.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://cdn.logo.com/hotlink-ok/logo-social.png', //Reemplazar con Icon de GUIO
+            height: 150,
+          ),
+          const Text("Guío"),
+          const SizedBox(height: 50),
+          const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Bienvenido"),
+                Text("Inicia sesión para disfrutar de todas \n"
+                    "las opciones que ofrece Guío"),
+              ]
+          ),
+          const SizedBox(height: 200),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 250,
+                height: 60,
+                child:ElevatedButton(
+                  onPressed: () {
+                    //Navigator.push(context,
+                      //MaterialPageRoute(builder: (context) => const LoginPage()),);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: const StadiumBorder(),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    backgroundColor: const Color.fromRGBO(65, 105, 225, 1),
+                  ),
+                  child: const Text(
+                    "Iniciar Sesión",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: 250,
+                height: 60,
+                child:ElevatedButton(
+                  onPressed: () {
+                    //Navigator.push(context,
+                      //MaterialPageRoute(builder: (context) => const SignupPage()),);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: const StadiumBorder(),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    backgroundColor: Colors.white,
+                    overlayColor: Colors.blue,
+                  ),
+                  child: const Text(
+                    "Registrate",
+                    style: TextStyle(fontSize: 20, color: Color.fromRGBO(65, 105, 225, 1)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
