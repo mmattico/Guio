f98f545b5e684cad3aa1c676bea4614550a4f7e0
mmattico <martinmatticoli@gmail.com>
2024-07-29 21:29:06 -0300

    MartinMatticoli

--- UnlamProyecto/Frontend/guio_proyecto/macos/Flutter/GeneratedPluginRegistrant.swift
+++ UnlamProyecto/Frontend/guio_proyecto/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import flutter_tts
 import path_provider_foundation
 import shared_preferences_foundation
 import speech_to_text_macos
+import text_to_speech_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  TextToSpeechMacOsPlugin.register(with: registry.registrar(forPlugin: "TextToSpeechMacOsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
