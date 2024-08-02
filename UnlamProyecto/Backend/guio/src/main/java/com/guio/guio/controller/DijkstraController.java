da7e7ee6bfbcbcfa9546b079316661b9ba3d3110
mmattico <martinmatticoli@gmail.com>
2024-08-02 18:12:50 -0300

    MartinMatticoliBaseDeDatos

--- UnlamProyecto/Backend/guio/src/main/java/com/guio/guio/controller/DijkstraController.java
+++ UnlamProyecto/Backend/guio/src/main/java/com/guio/guio/controller/DijkstraController.java
@@ -2,6 +2,7 @@ package com.guio.guio.controller;
 
 import com.guio.guio.model.*;
 import com.guio.guio.service.DijkstraService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,26 +12,31 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/dijktra")
 public class DijkstraController {
 
+	@Autowired
+	DijkstraService service;
+
 	@GetMapping("/mascorto")
 	/*
 	http://localhost:8080/api/dijktra/mascorto?ORIGEN=1&DESTINO=11
 	* */
 	public ResponseEntity<?> getCaminoMasCorto(@RequestParam(name = "ORIGEN") String nodoNombreOrigen,
 											   @RequestParam(name = "DESTINO") String nodoNombreDestino,
-											   @RequestParam(name = "PREFERENCIA") String preferencia) {
-		Grafo grafo = DijkstraService.obtenerGrafo(preferencia);
-		grafo = DijkstraService.calcularCaminoMasCortoDesdeFuente(grafo, nodoNombreOrigen, preferencia);
-		Camino camino = DijkstraService.convertirGrafoACamino(grafo, nodoNombreDestino);
+											   @RequestParam(name = "PREFERENCIA") String preferencia,
+											   @RequestParam(name = "UBICACION") String ubicacion) {
+		Grafo grafo = service.obtenerGrafo(ubicacion, preferencia);
+		grafo = service.calcularCaminoMasCortoDesdeFuente(grafo, nodoNombreOrigen);
+		Camino camino = service.convertirGrafoACamino(grafo, nodoNombreDestino);
 		return new ResponseEntity<>(camino, HttpStatus.OK);
 	}
 
 	@GetMapping("/portipo")
 	public ResponseEntity<?> getCaminoMasCortoPorTipo(@RequestParam(name = "ORIGEN") String nodoNombreOrigen,
 													  @RequestParam(name = "SERVICIO") String tipoNodoDestino,
-													  @RequestParam(name = "PREFERENCIA") String preferencia) {
-		Grafo grafo = DijkstraService.obtenerGrafo(preferencia);
-		grafo = DijkstraService.calcularCaminoMasCortoDesdeFuente(grafo, nodoNombreOrigen, preferencia);
-		Camino camino = DijkstraService.convertirGrafoACaminoPorTipo(grafo, tipoNodoDestino);
+													  @RequestParam(name = "PREFERENCIA") String preferencia,
+													  @RequestParam(name = "UBICACION") String ubicacion) {
+		Grafo grafo = service.obtenerGrafo(ubicacion, preferencia);
+		grafo = service.calcularCaminoMasCortoDesdeFuente(grafo, nodoNombreOrigen);
+		Camino camino = service.convertirGrafoACaminoPorTipo(grafo, tipoNodoDestino);
 		return new ResponseEntity<>(camino, HttpStatus.OK);
 	}
 
@@ -38,10 +44,10 @@ public class DijkstraController {
 	public ResponseEntity<?> getCaminoMasCortoPorTipo(@RequestParam(name = "ORIGEN") String nodoNombreOrigen,
 													  @RequestParam(name = "DESTINO") String nodoNombreDestino,
 													  @RequestParam(name = "SERVICIO") String tipoNodoIntermedio,
-													  @RequestParam(name = "PREFERENCIA") String preferencia) {
-		Grafo grafo = DijkstraService.obtenerGrafo(preferencia);
-		Grafo grafoServicio = DijkstraService.calcularCaminoMasCortoDesdeFuente(grafo, nodoNombreOrigen, preferencia);
-		Camino camino = DijkstraService.convertirGrafoACaminoConNodoIntermedio(grafoServicio, grafo, tipoNodoIntermedio, nodoNombreDestino, preferencia);
+													  @RequestParam(name = "PREFERENCIA") String preferencia,
+													  @RequestParam(name = "UBICACION") String ubicacion) {
+		Planificacion planificacion = service.obtenerPlanificacion(ubicacion, preferencia);
+		Camino camino = service.convertirGrafoACaminoConNodoIntermedio(planificacion, nodoNombreOrigen, tipoNodoIntermedio, nodoNombreDestino);
 		return new ResponseEntity<>(camino, HttpStatus.OK);
 	}
 }
---
+++ UnlamProyecto/Backend/guio/src/main/java/com/guio/guio/repositorio/NodoRepositorio.java
@@ -0,0 +1,15 @@
+package com.guio.guio.repositorio;
+
+import com.guio.guio.dao.NodoDAO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface NodoRepositorio  extends JpaRepository<NodoDAO, Long> {
+
+    @Query("SELECT n FROM NodoDAO n WHERE n.nombre = :nombre AND n.grafo.codigo = :ubicacion")
+    List<NodoDAO> findNodosByNombreAndGrafoUbicacion(@Param("nombre") String nombre, @Param("ubicacion") String ubicacion);
+
+}
