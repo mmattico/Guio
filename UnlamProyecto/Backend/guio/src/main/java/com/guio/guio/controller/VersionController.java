18e521cbc07389d6c7f7590b520a0e4463566263
mmattico <martinmatticoli@gmail.com>
2024-09-18 22:55:56 -0300

    Mejoras

--- UnlamProyecto/Backend/guio/src/main/java/com/guio/guio/controller/VersionController.java
+++ UnlamProyecto/Backend/guio/src/main/java/com/guio/guio/controller/VersionController.java
@@ -17,7 +17,7 @@ public class VersionController {
     public ResponseEntity<?> version (){
         Version version = new Version();
         version.setMensaje("OK");
-        version.setVersion("1.0.0.23");
+        version.setVersion("1.0.0.24");
         return new ResponseEntity<>(version, HttpStatus.OK);
     }
 }
