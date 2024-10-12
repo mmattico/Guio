39cb46a14c3df0ef0f6a9baff9105985304c6d07
mmatticoli <martinmatticoli@gmail.com>
2024-10-12 16:38:23 -0300

    MartinMatticoli

--- UnlamProyecto/Frontend/guio_proyecto/lib/pages/start_page.dart
+++ UnlamProyecto/Frontend/guio_proyecto/lib/pages/start_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
 import 'signup.dart';
+import 'auth_service.dart';
 //import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'location_selection.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:guio_proyecto/other/user_session.dart';
+import 'package:guio_proyecto/pages/change_password.dart';
+
+bool _isEmailRegistrado = false;
+bool passwordReset = false;
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final AuthService _authService = AuthService();
+
     return Scaffold(
       backgroundColor: Colors.white,
         body: Padding(
@@ -77,6 +90,39 @@ class StartPage extends StatelessWidget {
                         ),
                       ),
                     ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 55,
+                      child: IconButton(
+                        icon: Icon(FontAwesomeIcons.google, size: 30),
+                        onPressed: () async {
+                          User? user = await _authService.signInWithGoogle();
+                          _authService.signOut();
+                          if (user != null) {
+                            await _validateEmail(user.email.toString());
+                            if(_isEmailRegistrado){
+                              await _getUserByEmail(user.email.toString());
+                              if(passwordReset){
+                                Navigator.pushAndRemoveUntil(context,
+                                  MaterialPageRoute(builder: (context) => const ChangePassword()),
+                                      (Route<dynamic> route) => false,
+                                );
+                              } else {
+                                Navigator.pushAndRemoveUntil(context,
+                                  MaterialPageRoute(builder: (context) => LocationSelection()),
+                                      (Route<dynamic> route) => false,
+                                );
+                              }
+                            }else{
+                              Navigator.pushAndRemoveUntil(context,
+                                MaterialPageRoute(builder: (context) => SignupPage(selectedEmail: user.email.toString(), selectedUsuario: user.displayName.toString())),
+                                    (Route<dynamic> route) => false,);
+                            }
+                          }
+                        }
+                      ),
+                    ),
                   ],
                 ),
               ],
@@ -84,5 +130,39 @@ class StartPage extends StatelessWidget {
           ),
         )
     );
+  }
+
+  Future<void> _getUserByEmail(String email) async{
+    var url = Uri.https('guio-hgazcxb0cwgjhkev.eastus-01.azurewebsites.net', '/api/users/get-email/$email');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print("Response.body en _getUserByEmail: ${response.body}");
+      Map<String, dynamic> jsonMap = jsonDecode(response.body);
+      await saveUserID(jsonMap["usuarioID"]);
+      await saveUserFirstName(jsonMap["nombre"]);
+      await saveUserLastName(jsonMap["apellido"]);
+      await saveUserEmail(jsonMap["email"]);
+      await saveUserPhone(jsonMap["telefono"]);
+      await saveUserDNI(jsonMap["dni"]);
+      await saveUsername(jsonMap["usuario"]);
+      await saveUserAccessibility(jsonMap["accesibilidadDefault"]);
+      passwordReset = jsonMap["contraseÃ±aReseteada"];
+    }
+  }
+
+
+  Future<void> _validateEmail(String email) async {
+    var url = Uri.https('guio-hgazcxb0cwgjhkev.eastus-01.azurewebsites.net',
+        '/api/users/validar-correo', {'EMAIL': email});
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      var responseData = json.decode(response.body);
+      print("Retorno email $responseData");
+      if (!responseData) {
+        _isEmailRegistrado = false;
+      } else {
+        _isEmailRegistrado = true;
+      }
+    }
   }
 }
